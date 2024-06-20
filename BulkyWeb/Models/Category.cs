665346805edf676c5b482e70bc21da77800858a2
prajal77 <prajal.basnet0@gmail.com>
2024-06-20 22:28:26 +0545

    DataMigrated

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        //columns we want in category table
+
+        //using dataAnnotation to make Id as primary key
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
