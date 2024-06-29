b45ba1ab578ad69b3a8a5d26eac078c521855a41
prajal77 <prajal.basnet0@gmail.com>
2024-06-29 17:22:04 +0545

    EditDeleteAdded

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyWeb.Controllers
 {
@@ -25,14 +26,87 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //check category model
+            //Costume validation
+            /*if (obj.Name == obj.DisplayOrder.ToString() )
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+                // Clear model state to prevent model binding
+                ModelState.Clear();
+             }*/
             if (ModelState.IsValid)
             {
+               
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Can only work on primary Key
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Can work on any 
+          /*  Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault() ;*/
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Can only work on primary Key
+            Category? categoryFromDb = _db.Categories.Find(id);
+          
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        // if you want to use id only cannot use same name as parameter is same
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+                {
+                return NotFound();
+               
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+         
         }
 
     }
