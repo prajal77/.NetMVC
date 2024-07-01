16633880bace95ee7a5653f32f6dd4e09b5d881f
prajal77 <prajal.basnet0@gmail.com>
2024-07-01 20:03:38 +0545

    tosterNotificationAdded

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace BulkyWeb.Controllers
                
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 
@@ -71,6 +72,7 @@ namespace BulkyWeb.Controllers
 
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -104,6 +106,7 @@ namespace BulkyWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
          
